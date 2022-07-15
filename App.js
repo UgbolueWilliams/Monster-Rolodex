debfabb87b10d9b1bb38690a4f30e3bac4204409
UgbolueWilliams <ugboluewilliams@yahoo.com>
2022-07-15 10:48:46 +0100

    Monster Roldex Project

---
+++ App.js
@@ -0,0 +1,70 @@
+import {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+
+      monsters: [ ],
+      searchField: ''
+      
+   };
+
+
+  }
+
+  componentDidMount(){
+
+    fetch('https://jsonplaceholder.typicode.com/users') 
+    .then((response) => response.json())
+    .then((users) =>
+      this.setState(
+        () => {
+          return {monsters: users};
+        },
+        () =>{
+          console.log(this.state)
+        }
+      )
+    );
+
+  }
+
+  render(){
+
+    const filteredMonsters= this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+    return (
+      <div className="App">
+        <input className='search-box' 
+        type='search' 
+        placeholder='search monsters'
+         onChange={(event) => {
+          console.log(event.targevalue);
+          const searchField= event.target.value.toLocaleLowerCase();
+          
+          this.setState(() => {
+            return{searchField};
+          });
+        
+        }}
+         
+         />
+        {filteredMonsters.map((monster) =>{
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+
+  }
+  
+}
+
+export default App;
