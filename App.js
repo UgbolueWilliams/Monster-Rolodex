c59641400c22fccf1d453b8e206781c01bd685a9
UgbolueWilliams <ugboluewilliams@yahoo.com>
2022-07-20 14:38:10 +0100

    just completed the project

--- App.js
+++ App.js
@@ -50,7 +50,8 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <SearchBox className='monsters-search-box' onChangeHandler={onSearchChange} placeholder='search monsters'/>
+        <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox className='monsters-search' onChangeHandler={onSearchChange} placeholder='search monsters'/>
         <CardList monsters={filteredMonsters}/>
       </div>
     );
